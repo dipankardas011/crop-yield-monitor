932900af01b4e19fd6dad4f3f21dd2a21f0d9dba
Dipankar Das <dipankardas0115@gmail.com>
2023-11-26 14:26:48 +0530

    added the powershell scripts to run
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

---
+++ build.ps1
@@ -0,0 +1,21 @@
+
+cd recommendation && docker build -t recommend . && cd -
+
+cd imageprocessing && docker build -t image . && cd -
+
+cd authentication && docker build -t auth . && cd -
+
+cd lb-backend && docker build -t lb-backend . && cd -
+
+cd frontend && docker build -t frontend . && cd -
+
+cd db/image && docker build -t image-db . && cd -
+
+cd db/auth && docker build -t auth-db . && cd -
+
+cd db/recommend && docker build -t recommend-db . && cd -
+
+docker compose up
+
+# then visit the localhost:8080
+

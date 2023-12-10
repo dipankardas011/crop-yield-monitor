d490afb9a529d6f46fb5f7eda8006c44654b572a
rohan791 <97863521+rohan791@users.noreply.github.com>
2023-12-10 13:04:05 +0530

    Update build.ps1
    Signed-off-by: rohan791 <97863521+rohan791@users.noreply.github.com>

--- build.ps1
+++ build.ps1
@@ -1,19 +1,19 @@
 
-cd recommendation && docker build -t recommend . && cd -
+cd recommendation ; docker build -t recommend . ; cd 
 
-cd imageprocessing && docker build -t image . && cd -
+cd imageprocessing ; docker build -t image . ; cd ..
 
-cd authentication && docker build -t auth . && cd -
+cd authentication ; docker build -t auth . ; cd ..
 
-cd lb-backend && docker build -t lb-backend . && cd -
+cd lb-backend ; docker build -t lb-backend . ; cd ..
 
-cd frontend && docker build -t frontend . && cd -
+cd frontend ; docker build -t frontend . ; cd ..
 
-cd db/image && docker build -t image-db . && cd -
+cd db\image ; docker build -t image-db . ; cd ..\..
 
-cd db/auth && docker build -t auth-db . && cd -
+cd db\auth ; docker build -t auth-db . ; cd ..\..
 
-cd db/recommend && docker build -t recommend-db . && cd -
+cd db\recommend ; docker build -t recommend-db . ; cd ..\..
 
 docker compose up
 

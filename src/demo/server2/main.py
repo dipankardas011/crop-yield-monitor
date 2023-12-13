144efaf8a6b437ee54b8cc9a2b5238e9fcecd749
Dipankar Das <dipankardas0115@gmail.com>
2023-12-13 11:10:45 +0530

    added how to read header file
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- src/demo/server2/main.py
+++ src/demo/server2/main.py
@@ -6,15 +6,21 @@ CORS(app, resources={r"/upload": {"origins": "*"}})  # Enable CORS for all route
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
+    # Get headers
+    auth_header = request.headers.get('Authorization')
+    
+    # Get query parameters
+    username_param = request.args.get('username')
+    
+    print("username= ",username_param)
+    print("auth_header= ", auth_header)
+
     data = request.get_json()
     if not data or 'image' not in data or 'format' not in data:
         return jsonify({'error': 'Invalid request'}), 400
 
     image_data = bytes(data['image'])
     image_format = data['format']
-
-    print(image_format)
-    print(image_data)
 
     file_extension = 'jpeg' if image_format == 'image/jpeg' else 'png'
     file_name = f'image.{file_extension}'
@@ -26,7 +32,7 @@ def upload_image():
         print(e)
         return jsonify({'error': 'Failed to save image'}), 500
 
-    return jsonify({'status': 'Image uploaded'}), 200
+    return jsonify({'status': 'Image uploaded', 'crops': ['crop', 'maize']}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
